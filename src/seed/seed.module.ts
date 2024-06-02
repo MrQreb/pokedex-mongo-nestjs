b8e8799a9972c1f3047f6a276f7045f766e4a13d
Jesus Eduardo Arias Diaz <devmr.qreb@outlook.com>
2024-06-02 15:00:32 -0600

    imported PokemonModule to create Pokemons

--- src/seed/seed.module.ts
+++ src/seed/seed.module.ts
@@ -2,8 +2,16 @@ import { Module } from '@nestjs/common';
 import { SeedService } from './seed.service';
 import { SeedController } from './seed.controller';
 
+//Importamos el modulo de pokemon para el seed
+import { PokemonModule } from 'src/pokemon/pokemon.module';
+
 @Module({
   controllers: [SeedController],
   providers: [SeedService],
+
+  imports: [
+    //Importamos el modulo de pokemon para poder usarlo en el seed
+    PokemonModule 
+  ]
 })
 export class SeedModule {}
