21c6647a31c872b93a6b12d16da0cc5f3d5de723
Jesus Eduardo Arias Diaz <devmr.qreb@outlook.com>
2024-06-02 14:59:58 -0600

    executeSeed method added

--- src/seed/seed.controller.ts
+++ src/seed/seed.controller.ts
@@ -1,8 +1,7 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { SeedService } from './seed.service';
 
-import axios, { AxiosInstance } from 'axios';
-import { PokeResponse } from './interfaces/poke-reponse.interface';
+
 
 
 
@@ -10,31 +9,19 @@ import { PokeResponse } from './interfaces/poke-reponse.interface';
 export class SeedController {
   
   constructor(
+    //Inyectamos el servicio
     private readonly seedService: SeedService,
 
   ) {}
 
-  //Dependencia del servio para hacer peticiones http
-  private readonly axios:AxiosInstance =  axios;
-
-
   @Get()
-  async executeSeed() {
-    const { data } = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=10');
-
-    //data.results => Array de objetos con la informacion de los pokemones
-    //desestructuramos el objeto para obtener el nombre y la url
-
-    data.results.forEach( ( {name, url }) =>{
-      const spaces = url.split('/');
-     
-     const id:number = +spaces[ spaces.length - 2 ] ;
-  
-     
-    })
-
-    return data.results;
+  executeSeed() {
+    //Ejecutamos el metodo del servicio
+    return this.seedService.executeSeed();
   }
+
+
+  
 
   
 }
