a2d52f265889dae794d0858b8c50dadc4436ddfd
Jesus Eduardo Arias Diaz <devmr.qreb@outlook.com>
2024-06-02 20:54:23 -0600

    common.module.ts exported axiosAdapter and Provider Axios

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,4 +1,18 @@
 import { Module } from '@nestjs/common';
 
-@Module({})
+import { AxiosAdapter } from './adapters/axios.adapter';
+
+
+ 
+
+
+
+@Module({
+    
+    //Exportamos el servicio para poder inyectarlo en otros modulos
+    providers: [AxiosAdapter],
+
+    //Exportamos el servicio para poder inyectarlo en otros modulos
+    exports: [AxiosAdapter]
+})
 export class CommonModule {}
