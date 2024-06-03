86b998e8a5b25998a4b68b7fdce429e998970341
Jesus Eduardo Arias Diaz <devmr.qreb@outlook.com>
2024-06-02 20:52:53 -0600

    seed.Module.ts imported PokemonModule and CommonModule to use adapter

--- src/seed/seed.module.ts
+++ src/seed/seed.module.ts
@@ -4,6 +4,8 @@ import { SeedController } from './seed.controller';
 
 //Importamos el modulo de pokemon para el seed
 import { PokemonModule } from 'src/pokemon/pokemon.module';
+import { CommonModule } from 'src/common/common.module';
+
 
 @Module({
   controllers: [SeedController],
@@ -11,7 +13,12 @@ import { PokemonModule } from 'src/pokemon/pokemon.module';
 
   imports: [
     //Importamos el modulo de pokemon para poder usarlo en el seed
-    PokemonModule 
+    PokemonModule ,
+
+
+    //Importamos el modulo de common para poder usarlo en el seed 
+    //La parte de axios
+    CommonModule
   ]
 })
 export class SeedModule {}
